f22859e57ad670d8fb5f9c67cd77e7c2287f6c52
Ian Silva Santos <ian.santos@sptech.school>
2025-02-10 21:55:06 -0300

    feat: Criando subnet e vinculando-a a vpc.

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -7,3 +7,13 @@ resource "aws_vpc" "bridee_vpc"{
     Name = "bridee_vpc"
   }
 }
+
+resource "aws_subnet" "bridee_public_subnet"{
+  vpc_id = aws_vpc.bridee_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    IAC = true
+    Name = "bridee_public_subnet"
+  }
+}
