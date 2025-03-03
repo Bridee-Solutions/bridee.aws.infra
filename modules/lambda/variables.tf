83acb13e63d227004289fd38acfee318c392f8da
ian15963 <ian.santos@sptech.school>
2025-03-03 10:53:13 -0300

    feat: Criando as variáveis

---
+++ modules/lambda/variables.tf
@@ -0,0 +1,17 @@
+variable private_security_group_id {
+  type        = string
+  default     = ""
+  description = "Id do grupo de segurança dos recursos da subnet privada"
+}
+
+variable private_subnet_id {
+  type        = string
+  default     = ""
+  description = "Id da subnet privada"
+}
+
+variable wedding_job_image_uri {
+  type        = string
+  default     = ""
+  description = "Uri da imagem do job na ECR"
+}
