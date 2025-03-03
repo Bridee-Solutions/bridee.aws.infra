ce565184234b32e7aba6944711d58bdebcb4f34e
ian15963 <ian.santos@sptech.school>
2025-03-02 22:39:42 -0300

    feat: Configurando bucket para hospedagem do front.

--- modules/s3/s3user.tf
+++ modules/s3/s3user.tf
@@ -6,3 +6,28 @@ resource "aws_s3_bucket" "s3_user" {
     IAC = true
   }
 }
+
+resource "aws_s3_bucket" "s3_website" {
+  bucket = "bridee_ui"
+
+  tags = {
+    Name = "bridee_ui",
+    IAC = true
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "bridee_website"{
+  bucket = aws_s3_bucket.s3_website.id
+
+  index_document{
+    suffix = "index.html"
+  }
+
+  error_document{
+    key = "index.html"
+  }
+
+  tags = {
+    IAC = true
+  }
+}
