ca7ffabb0fac923e1fd0ea9807158fed109fb46b
ian15963 <ian.santos@sptech.school>
2025-03-03 08:58:55 -0300

    feat: Deixando comando para proteção de deleção comentado.

--- modules/s3/s3user.tf
+++ modules/s3/s3user.tf
@@ -1,6 +1,10 @@
 resource "aws_s3_bucket" "s3_user" {
   bucket = "bridee_user_bucket"
 
+  # lifecycle{
+  #   prevent_destroy = true
+  # }
+
   tags = {
     Name = "bridee_user",
     IAC = true
