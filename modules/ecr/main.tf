3011fe15f4893f8c838fde551775e230251e8880
ian15963 <ian.santos@sptech.school>
2025-03-22 09:56:48 -0300

    feat: Creando configuração do ECR

---
+++ modules/ecr/main.tf
@@ -0,0 +1,8 @@
+resource "aws_ecr_repository" "bridee_repository"{
+    name = "bridee-repository"
+    image_tag_mutability = "IMMUTABLE"
+
+    image_scanning_configuration{
+        scan_on_push = true
+    }
+}
